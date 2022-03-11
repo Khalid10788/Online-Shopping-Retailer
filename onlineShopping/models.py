37c11b61c7a4f943b0e50ab2ebc0c24c7ebdef78
khalid <weblance10788@gmail.com>
2022-03-11 10:55:02 -0800

    2nd change

--- onlineShopping/models.py
+++ onlineShopping/models.py
@@ -8,23 +8,22 @@ from django_countries.fields import CountryField
 
 
 class pcategory(models.Model):
-    c_name= models.CharField(max_length=50,unique=True)
+    c_name= models.CharField(max_length=50)
     c_description= models.TextField(max_length=500)
     thumbnail= models.ImageField(upload_to ='uploads/',default='Category_default.jpg')
     def __str__(self):
         return self.c_name
 
 class brand(models.Model):
-    b_name= models.CharField(max_length=50, unique=True)
+    b_name= models.CharField(max_length=50)
     b_description= models.TextField(max_length=500)
     thumbnail= models.ImageField(upload_to ='uploads/',default='brand_default.jpg')
     def __str__(self):
         return self.b_name
 
 class product(models.Model):
-    pcate= models.ForeignKey(pcategory, on_delete=models.CASCADE)
-    pbrand= models.ForeignKey(brand, on_delete=models.CASCADE)
-    pname =models.CharField(max_length=50,unique=True)
+   
+    pname =models.CharField(max_length=50)
     pdescription =models.TextField(max_length=500)
     psalep=models.IntegerField()
     pdiscountp= models.IntegerField()
@@ -45,7 +44,7 @@ class product_brand(models.Model):
 
 class customer(models.Model):
 
-    customer= models.ForeignKey(customer, on_delete=models.CASCADE)
+    customer= models.ForeignKey(User, on_delete=models.CASCADE)
     phone= models.CharField(max_length=25)
     billing_address= models.TextField(max_length=400)
     shipping_address= models.TextField(max_length=400)
@@ -55,7 +54,7 @@ class cart(models.Model):
     customer= models.ForeignKey(customer, on_delete=models.CASCADE)
     prd= models.ForeignKey(product, on_delete=models.CASCADE,unique=True)
     qty= models.IntegerField(default=1)
-    totalAmount=models.DecimalField(default=0)
+    totalAmount=models.DecimalField(max_digits=19, decimal_places=10)
 
 
 class order(models.Model):
@@ -69,19 +68,19 @@ class order(models.Model):
       )
    
     customerid= models.ForeignKey(customer, on_delete=models.CASCADE)
-    amount = models.DecimalField()
+    amount = models.DecimalField(max_digits=19, decimal_places=10)
     order_address= models.TextField(max_length=400)
     shipping_address= models.TextField(max_length=400)
     order_email=models.EmailField()
     orderdate=models.DateTimeField()
-    order_status= models.ChoiceField(choices = order_CHOICES )
+    order_status= models.CharField( max_length=50,choices = order_CHOICES )
 
 
 class order_detail(models.Model):
     order_id= models.ForeignKey(order, on_delete=models.CASCADE)
-    product_id= models.ForeignKey(product, on_delete=models.CASCADE,unique=True)
+    product_id= models.ForeignKey(product, on_delete=models.CASCADE)
     qty= models.IntegerField(default=1)
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=19, decimal_places=10)
 
 
 
